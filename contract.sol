c07933560338c520f64141a6a72db46f31ed7e00
TvoiiSon <32959031+AndreyLastovetskiy@users.noreply.github.com>
2022-10-11 11:57:39 +0300

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -5,9 +5,10 @@ contract ShopManager {
 
     //Структура ролей в системе
     enum Role {
-        OWNER,
+        BUYER,
         SELLER,
-        BUYER
+        SHOP,
+        OWNER
     }
     Role currentRole;
 
@@ -17,8 +18,20 @@ contract ShopManager {
     struct Shop {
         string name;
         address owner;
-        uint32[] products;
+        uint256 allowedToCapture;
         bool exists;
+    }
+
+    mapping(string => ShopProducts) shopProductCounts;
+    
+    struct ShopProducts {
+        string shop;
+        mapping(uint256 => ShopProductCount) products;
+    }
+
+    struct ShopProductCount {
+        uint256 id;
+        uint256 amount;
     }
 
     //Структура пользователей в системе
@@ -30,6 +43,23 @@ contract ShopManager {
         Role maxRole;
         Role currentRole;
         bool exists;
+    }
+
+    mapping(address => UserProducts) userProducts;
+
+    struct UserProducts {
+        address user;
+        mapping(string => UserShopProducts) shops;
+    }
+
+    struct UserShopProducts {
+        string shop;
+        mapping(uint256 => UserOwnedProduct) products;
+    }
+
+    struct UserOwnedProduct {
+        uint256 id;
+        uint256 amount;
     }
 
     //Структура заявки на повышение роли
@@ -52,16 +82,27 @@ contract ShopManager {
 
     struct BuyRequest {
         address author;
-        address shop;
         uint256 productId;
         uint256 amount;
+        string shop;
         bool accepted;
         bool reviewed;
         bool exists;
     }
+
+    struct RefundRequest {
+        uint256 id;
+        uint256 buyRequestId;
+        address author;
+        bool reviewed;
+        bool accepted;
+        bool exists;
+    }
+    mapping(uint256 => RefundRequest) refundRequests;
+    uint256[] refundRequestIds;
+
     mapping(uint256 => BuyRequest) BuyRequests;
-    mapping(address => BuyRequest) AddressRequest;
-    address[] AddressRequests;
+    uint256[] buyRequestIds;
 
     // Маппинг и массив для работы со всеми структурами ПОЛЬЗОВАТЕЛЕЙ
     mapping(address => User) users;
@@ -79,6 +120,7 @@ contract ShopManager {
 
     // Магазины
     mapping(string => Shop) shops;
+    mapping(address => string) shopNamesByAddress;
     string[] shopCities;
 
     //Модификатор для работы функции, которую может запустить только владелец системы OWNER
@@ -95,6 +137,11 @@ contract ShopManager {
 
     modifier onlyShopSeller(string memory shopCity) {
         require(keccak256(abi.encodePacked(users[msg.sender].shop)) == keccak256(abi.encodePacked(shopCity)), "You have to be a seller in required shop");
+        _;
+    }
+
+    modifier onlyShopOwner() {
+        require(users[msg.sender].currentRole == Role.SHOP);
         _;
     }
 
@@ -230,20 +277,22 @@ contract ShopManager {
     }
 
     //Функция создания заявки на покупку товара в магазине
-    function buyProduct(address shop, address sender, uint256 productId, uint256 amount) public payable returns(bool) {
-        BuyRequests[productIds.length] = BuyRequest(
+    function buyProduct(string memory shop, address sender, uint256 productId, uint256 amount) public payable returns(bool) {
+        BuyRequests[buyRequestIds.length] = BuyRequest(
             sender,
-            shop,
             productId,
             amount,
+            shop,
             false,
             false,
             true 
         );
-        AddressRequests.push(sender);
 
         uint256 amountToPay = amount * productsPrice;
         require(msg.value == amountToPay, "You need to send exact amount of ether that is required to buy product");
+
+        require(shopProductCounts[shop].products[productId].amount >= amount, "This shop does not have this item");
+        shopProductCounts[shop].products[productId].amount -= amount;
 
         return true;
     }
@@ -254,29 +303,82 @@ contract ShopManager {
     }
 
     //Функция, которая подтверждает покупку товара(подтвержает SELLER)
-    function acceptedBuyProduct(address sender) public onlySeller returns(bool) {
-        AddressRequest[sender].accepted = true;
-        AddressRequest[sender].reviewed = true;
+    function acceptedBuyProduct(uint256 requestId) public onlySeller returns(bool) {
+        BuyRequests[requestId].reviewed = true;
+        BuyRequests[requestId].accepted = true;
+        BuyRequest memory buyRequest = BuyRequests[requestId];
+
+        userProducts[buyRequest.author].shops[buyRequest.shop].products[buyRequest.productId].amount += buyRequest.amount;
 
         return true;
     }
 
     //Функция, которая отменяет покупку товара(подтвержает SELLER)
-    function cancelBuyProduct(uint256 productId) public onlySeller returns(bool) {
-        BuyRequests[productId].reviewed = true;
+    function cancelBuyProduct(uint256 requestId) public onlySeller returns(bool) {
+        BuyRequests[requestId].reviewed = true;
+        BuyRequest memory buyRequest = BuyRequests[requestId];
+
+        shopProductCounts[buyRequest.shop].products[buyRequest.productId].amount += buyRequest.amount;
 
         return true;
     }
 
     //Функция, которая отменяет покупку товара(отменяет BUYER)
-    function cancelRequestBuyProduct(uint256 productId) public onlyBuyer returns(bool) {
-        BuyRequests[productId].exists = false;
+    function cancelRequestBuyProduct(uint256 requestId) public onlyBuyer returns(bool) {
+        BuyRequests[requestId].exists = false;
+        BuyRequest memory buyRequest = BuyRequests[requestId];
+
+        shopProductCounts[buyRequest.shop].products[buyRequest.productId].amount += buyRequest.amount;
 
         return true;
     }
 
-        constructor() {
+    function createRefundRequest(uint256 buyRequestId) public onlyBuyer returns(bool) {
+        BuyRequest memory buyRequest = BuyRequests[buyRequestId];
+        require(buyRequest.author == msg.sender, "You are not permitted to do this");
+        require(shops[buyRequest.shop].allowedToCapture >= buyRequest.amount * productsPrice, "Shop does not have enough ether for you to refund");
+        shops[shopNamesByAddress[msg.sender]].allowedToCapture -= buyRequest.amount * productsPrice;
 
+        refundRequests[refundRequestIds.length] = RefundRequest(refundRequestIds.length, buyRequestId, msg.sender, false, false, true);
+        refundRequestIds.push(refundRequestIds.length);
+
+        return true;
+    }
+
+    function acceptRefundRequest(uint256 refundRequestId) public onlySeller returns(bool) {
+        refundRequests[refundRequestId].accepted = true;
+        refundRequests[refundRequestId].reviewed = true;
+
+        BuyRequest memory buyRequest = BuyRequests[refundRequests[refundRequestId].buyRequestId];
+        payable(refundRequests[refundRequestId].author).transfer(buyRequest.amount * productsPrice);
+
+        return true;
+    }
+
+    function denyRefundRequest(uint256 refundRequestId) public onlySeller returns(bool) {
+        refundRequests[refundRequestId].reviewed = true;
+        
+        BuyRequest memory buyRequest = BuyRequests[refundRequests[refundRequestId].buyRequestId];
+        shops[buyRequest.shop].allowedToCapture += buyRequest.amount * productsPrice;
+        return true;
+    }
+
+    // отменить запрос на возврат от имени покупателя
+    function cancelRefundRequest(uint256 refundRequestId) public onlyBuyer {
+        refundRequests[refundRequestId].exists = false;
+
+        BuyRequest memory buyRequest = BuyRequests[refundRequests[refundRequestId].buyRequestId];
+        shops[buyRequest.shop].allowedToCapture += buyRequest.amount * productsPrice;
+    }
+
+    function capture(uint256 amount) public onlyShopOwner {
+        require(shops[shopNamesByAddress[msg.sender]].allowedToCapture >= amount, "You can't capture more than its allowed for you to capture");
+
+        shops[shopNamesByAddress[msg.sender]].allowedToCapture -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    constructor() {
         users[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = User (
             "Owner",
             "",
@@ -286,16 +388,20 @@ contract ShopManager {
             Role.OWNER,
             true
         );
+        userLogins["Owner"] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+        userLoginsArray.push("Owner");
 
         users[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = User (
             "Seller",
-            "",
+            "TSUM",
             0x1841d653f9c4edda9d66a7e7737b39763d6bd40f569a3ec6859d3305b72310e6,
             0x64e604787cbf194841e7b68d7cd28786f6c9a0a3ab9f8b0a0e87cb4387ab0107,
             Role.SELLER,
             Role.SELLER,
             true
         );
+        userLogins["Seller"] = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        userLoginsArray.push("Seller");
 
         users[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = User (
             "Buyer",
@@ -306,6 +412,44 @@ contract ShopManager {
             Role.BUYER,
             true
         );
+        userLogins["Buyer"] = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+        userLoginsArray.push("Buyer");
+
+        shops["Moscow"] = Shop (
+            "TSUM",
+            0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,
+            0,
+            true
+        );
+        shopNamesByAddress[0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB];
+        shopCities.push("Moscow");
+
+        shops["SPb"] = Shop (
+            "ohMyVape",
+            0x617F2E2fD72FD9D5503197092aC168c91465E7f2,
+            0,
+            true
+        );
+        shopNamesByAddress[0x617F2E2fD72FD9D5503197092aC168c91465E7f2];
+        shopCities.push("SPb");
+
+        shops["Tgn"] = Shop (
+            "Zvezdnii TGN",
+            0x17F6AD8Ef982297579C203069C1DbfFE4348c372,
+            0,
+            true
+        );
+        shopNamesByAddress[0x17F6AD8Ef982297579C203069C1DbfFE4348c372];
+        shopCities.push("Tgn");
+
+        shops["Magadan"] = Shop (
+            "Stvoli v Magadane",
+            0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678,
+            0,
+            true
+        );
+        shopNamesByAddress[0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678];
+        shopCities.push("Magadan");
     }
 
 }
